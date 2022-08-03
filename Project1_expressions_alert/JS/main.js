bf9175f81b7dbfcc6109f1e7320588a4b1bd7b15
Cindyisawsome <106400662+Cindyisawsome@users.noreply.github.com>
2022-08-03 10:55:53 +0800

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("This is the first one!")
+document.write("double kill!")
+var A=("blue"), B =("bird")  //difining the var
+var A=A.fontcolor("red")     //difining the color i would like 
+var B=B.fontcolor("green")
+document.write(A+B)
+
+function My_First_Function() {  //defining a function and naming it
+    var str = "This is green!";
+    var result =str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
