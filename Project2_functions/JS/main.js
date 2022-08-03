c0c4107d8cb42f77ff79336fa368a9a011050bf2
Cindyisawsome <106400662+Cindyisawsome@users.noreply.github.com>
2022-08-04 01:08:16 +0800

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("are you ready?")
+var bob= ("3"), cindy=("#"), amy=("6")
+document.write(bob+cindy+amy)
+function My_Second_Function() {
+    var sentence="Maybe go somewhere..";
+    sentence += "ops, I am still here!";
+    var sentence=sentence.fontcolor("blue");
+    document.getElementById("Concatenate").innerHTML=sentence;
+
+}
