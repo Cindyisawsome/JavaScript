8c6209d9d1aa879c286193ea89aa11bb6ce4ebc4
Cindyisawsome <106400662+Cindyisawsome@users.noreply.github.com>
2022-08-06 12:20:35 +0800

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/Example.js
@@ -0,0 +1,9 @@
+function My_Function(){
+    document.getElementById("Test").innerHTML = 0/0;
+}
+function My_Function1(){
+    document.getElementById("Test").innerHTML = isNaN('This is a string');
+}
+function My_Function2(){
+    document.getElementById("Test").innerHTML = isNaN('007');
+}
---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,28 @@
+document.write(typeof "56");
+document.write(typeof 4);
+
+
+
+function My_Function(){
+    document.getElementById("Infinity").innerHTML = (4E502);
+}
+
+function My_Function2(){
+    document.getElementById("Boolean").innerHTML = (52>32);
+}
+function My_Function3(){
+    document.getElementById("Equal").innerHTML = (52==52);
+}
+document.write(8+"205");
+
+function My_Function4(){
+    document.getElementById("Logical Operators").innerHTML = (52>32 || 62>32);
+}
+function My_Function5(){
+    document.getElementById("Not Operators").innerHTML = !(62>32);
+}
+function My_Function6(){
+    X= 7
+    Y= 9
+    document.getElementById("Triple Equal").innerHTML = (X === Y);
+}
