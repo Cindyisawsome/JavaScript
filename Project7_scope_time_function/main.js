f21ee2a8c6250409eab3da42dcdf8d5126dd6f0d
Cindyisawsome <106400662+Cindyisawsome@users.noreply.github.com>
2022-08-08 08:53:43 +0800

    Add files via upload

---
+++ Project7_scope_time_function/main.js
@@ -0,0 +1,20 @@
+var X = 66;
+function divide_1() {
+    document.getElementById("Divide1").innerHTML = (X/3);
+}
+function divide_2() {
+    document.getElementById("Divide2").innerHTML =(X/2);
+}
+divide_1();
+divide_2();
+
+function divide_3() {
+    var X = 99
+    document.getElementById("Divide3").innerHTML=(X/3);
+}
+function divide_4() {
+    
+    document.getElementById("Divide4").innerHTML=(X/2);
+}
+divide_3();
+divide_4();
